40e8fe45fbb7dedae2ec4abf38c7327bb68fa2b8
tony.baines.1@gmail.com <>
2010-10-26 21:52:06 +0100

    savepoint

--- src/test/java/kata/bowling/BowlingGameTest.java
+++ src/test/java/kata/bowling/BowlingGameTest.java
@@ -1,76 +1,82 @@
 package kata.bowling;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
 import org.junit.Test;
 
-
 public class BowlingGameTest {
-  @Test public void shouldScoreAGameOfOneFrameWhereNotAllPinsAreKnockedDown() {
-    BowlingGame game = new BowlingGame();
+  private BowlingGame game = new BowlingGame();
+
+  @Test
+  public void shouldScoreAGameOfOneFrameWhereNotAllPinsAreKnockedDown() {
     game.bowl(5);
     game.bowl(4);
-    bowlNoScoreFrames(game, 9);
+    bowlNoScoreFrames(9);
     assertEquals(9, game.score());
   }
 
-  @Test public void shouldScoreAFrameWhereAllThePinsAreKnockedDownByTheFirstBall() {
-    BowlingGame game = new BowlingGame();
+  @Test
+  public void shouldScoreAGameWhereOnlyOnePinIsKnockedDownWithEachBowl() {
+    bowlManyTimes(1, 20);
+    assertEquals(20, game.score());
+  }
+
+  @Test
+  public void shouldScoreAFrameWhereAllThePinsAreKnockedDownByTheFirstBall() {
     game.bowl(10);
-    bowlNoScoreFrames(game, 9);
+    bowlNoScoreFrames(9);
     assertEquals(10, game.score());
   }
-  
-  @Test public void shouldScoreAPairOfFramesWhereAStrikeIsFollowedByJustOneKnockedDownInTheNext() {
-    BowlingGame game = new BowlingGame();
+
+  @Test
+  public void shouldScoreAPairOfFramesWhereAStrikeIsFollowedByJustOneKnockedDownInTheNext() {
     game.bowl(10);
     game.bowl(1);
     game.bowl(0);
-    bowlNoScoreFrames(game, 8);
-    assertEquals(12, game.score());
-  }
-  
-  @Test public void shouldScoreAPairOfFramesWhereASpareIsFollowedByJustOneKnockedDownInTheFirstBallOfTheNext() {
-    BowlingGame game = new BowlingGame();
-    game.bowl(5);
-    game.bowl(5);
-    game.bowl(1);
-    game.bowl(0);
-    bowlNoScoreFrames(game, 8);
+    bowlNoScoreFrames(8);
     assertEquals(12, game.score());
   }
 
-  @Test public void shouldScoreAPairOfFramesWhereASpareIsFollowedByJustOneKnockedDownInTheSecondBallOfTheNext() {
-    BowlingGame game = new BowlingGame();
+  @Test
+  public void shouldScoreAPairOfFramesWhereASpareIsFollowedByJustOneKnockedDownInTheFirstBallOfTheNext() {
+    game.bowl(5);
+    game.bowl(5);
+    game.bowl(1);
+    game.bowl(0);
+    bowlNoScoreFrames(8);
+    assertEquals(12, game.score());
+  }
+
+  @Test
+  public void shouldScoreAPairOfFramesWhereASpareIsFollowedByJustOneKnockedDownInTheSecondBallOfTheNext() {
     game.bowl(5);
     game.bowl(5);
     game.bowl(0);
     game.bowl(1);
-    bowlNoScoreFrames(game, 8);
+    bowlNoScoreFrames(8);
     assertEquals(11, game.score());
   }
-  
-  @Test public void shouldScoreAGameWhereNineStrikesAreBowledInARow() {
-    BowlingGame game = new BowlingGame();
-    for (int i = 1; i <= 9; i++) {
-      game.bowl(10);
-    }
-    bowlNoScoreFrames(game, 1);
+
+  @Test
+  public void shouldScoreAGameWhereNineStrikesAreBowledInARow() {
+    bowlManyTimes(10, 9);
+    bowlNoScoreFrames(1);
     assertEquals(240, game.score());
   }
-  
-  @Test public void shouldScoreAGameWhereThirteenStrikesAreBowledInARow() {
-    BowlingGame game = new BowlingGame();
-    for (int i = 1; i <= 13; i++) {
-      game.bowl(10);
-    }
+
+  @Test
+  public void shouldScoreAGameWhereThirteenStrikesAreBowledInARow() {
+    bowlManyTimes(10, 13);
     assertEquals(300, game.score());
   }
-  
-  private void bowlNoScoreFrames(BowlingGame game, int noScoreFrames) {
-    for(int i = 1; i <= noScoreFrames; i++) {
-      game.bowl(0);
-      game.bowl(0);
+
+  private void bowlManyTimes(int score, int times) {
+    for (int i = 1; i <= times; i++) {
+      game.bowl(score);
     }
+  }
+
+  private void bowlNoScoreFrames(int noScoreFrames) {
+    bowlManyTimes(0, noScoreFrames);
   }
 }
